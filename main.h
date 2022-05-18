f71550761c66c7df67827c46f65725872f900341
kalu_coder <awesomecoding51@gmail.com>
2022-05-18 22:00:40 +0300

    betty style checked

--- main.h
+++ main.h
@@ -43,7 +43,8 @@ typedef struct buffer_s
 typedef struct converter_s
 {
 	unsigned char specifier;
-	unsigned int (*func)(va_list, buffer_t*);
+	unsigned int (*func)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 } converter_t;
 
 /**
@@ -92,7 +93,8 @@ unsigned char handle_flags(const char *flags, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t*);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
 
 /* Modifiers */
 unsigned int print_width(buffer_t *output, unsigned int printed,
